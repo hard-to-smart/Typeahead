bdb60fb69434d3ff7da674a10c517732c03cca79
INNO <2403punjabidivya@gmail.com>
2024-10-01 12:54:43 +0530

    to resolve handleOptionsSelect

--- src/Typehead.jsx
+++ src/Typehead.jsx
@@ -1,53 +1,97 @@
 import React, { useEffect, useRef, useState } from "react";
 import { UPIids } from "./UpiLists";
 export const Typehead = () => {
+  // take input
   const [inputValue, setInputValue] = useState("");
+  // setting options
   const [optionsValue, setOptionsValue] = useState([]);
-  const [displayParseValue, setDisplayParseValue] = useState("");
-  const [focus, setFocus] = useState("");
-    const inputRef = useRef(null);
+  // displaying the options in the span
+  const [displayParseValue, setDisplayParseValue] = useState('');
+  const [currentSuggestion, setCurrentSuggestion] = useState(UPIids[1]);
+  const [onFocus, setFocus] = useState(false);
+  //this sets input value
   const handleInputValue = (e) => {
     setInputValue(e.target.value);
   };
 
-  const handleOptionSelect = (e) => {
-    setDisplayParseValue(e.target.innerText);
-    setInputValue(inputValue.concat(e.target.innerText));
-    inputRef.current.focus()
-  };
+ 
 
-const handleOptionsValue = () => {
+  const handleOptionsValue = () => {
     setOptionsValue(
-    UPIids.filter((value) => {
-      if (inputValue.match(/^(.+@)/)) {
-        return value;
-      }
-    })
+      UPIids.filter((id) => {
+        if (inputValue.match(/^(.+@)/)) {
+          let check = id;
+          if (check) {
+            const returnVal = id.startsWith(inputValue.split("@")[1], id);
+            console.log(returnVal);
+            return returnVal;
+            setFocus(true);
+
+          }
+          return id;
+        }
+      })
     );
   };
 
-  useEffect(()=>{
+  const handleOptionSelect = (e) => {
+    if(e.keyCode === 40) { //down arrow
+     
+      
+      let pointerIndex = optionsValue.indexOf(currentSuggestion);
+      setCurrentSuggestion(optionsValue[pointerIndex+1])
+      
+      // setCurrentSuggestion(optionsValue[pointerIndex+1])
+     
+    }
+    if(e.keyCode === 38){
+
+    }
+
+    if(e.keyCode === 13 && focus){
+
+    }
+    
+    setCurrentSuggestion(optionsValue.at(0));
+    setDisplayParseValue(currentSuggestion);
+  };
+
+
+  useEffect(() => {
     handleOptionsValue();
-  },[inputValue])
+    
+  }, [inputValue]);
 
   return (
     <>
       <div className="input-parent">
+      
+        <div className="input-main">
+        { setFocus && 
+        <input style={{zIndex:1, position:"absolute", width:"400px", height:"50px", fontSize:"large", border:'4px solid transparent'}} value={inputValue.split("@")[0]+"@" + displayParseValue} disabled/>
+        }
         <input
           name="upi"
-          ref={inputRef}
           value={inputValue}
           autoFocus
-          placeholder="Enter Upi Id"
+          className="input-main-sub"
+          // placeholder="Enter Upi Id"
           onChange={handleInputValue}
+          onKeyDown={handleOptionSelect}
+          style={{zIndex:2}}
         />
-        <span className="">{displayParseValue}</span>
-      </div>
+        </div>
       <ul>
-        {optionsValue.map((value, index) => (
-          <li key={index} onClick={handleOptionSelect}>{value}</li>
+        {optionsValue.map((value) => (
+          <li
+            key={value}
+            className={`${value === currentSuggestion && "list-hover"}`}  >
+            {value}
+          </li>
         ))}
       </ul>
+      </div>
+
     </>
   );
 };
